e0b0f3788184cb3a8530cef5025ac7c49078c1c4
Ashesha <kovvsevolod@yandex.ru>
2024-10-13 23:39:56 +0700

    Updated 5 pack of tasks

--- 1_sem/homeworks/5_pack/2_bincoeff/solution.c
+++ 1_sem/homeworks/5_pack/2_bincoeff/solution.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 #define MAX_N 1500
 
-double pascal_triangle[MAX_N + 1][MAX_N + 1];
-
-void build_pascal_triangle() {
+void build_pascal_triangle(double pascal_triangle[MAX_N + 1][MAX_N + 1]) {
     for (int i = 0; i < MAX_N; i++)
     {
         for (int j = 0; j <= i; j++)
@@ -28,11 +26,14 @@ int main()
 
     int Q, n, k;
 
-    build_pascal_triangle();
+    double pascal_triangle[MAX_N + 1][MAX_N + 1];
+
+    build_pascal_triangle(pascal_triangle);
 
     scanf("%d", &Q);
 
-    for (int i = 0; i < Q; i++) {
+    for (int i = 0; i < Q; i++)
+    {
         scanf("%d %d", &n, &k);
         printf("%0.10g\n", pascal_triangle[n][k]);
     }
